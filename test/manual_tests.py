c3a3703df433f25e335857f8546c43e884bff5cd
Adrian Samoticha <redacted-for@privacy-reasons.com>
2021-07-07 20:40:10 +0200

    feat: add second manual test

--- test/manual_tests.py
+++ test/manual_tests.py
@@ -17,7 +17,7 @@ class ManualTest(unittest.TestCase):
 		except OSError as e:
 			print("Directory deletion failed: %s - %s." % (e.filename, e.strerror))
 	
-	def test_manually(self):
+	def test_manually_1(self):
 		PATH = "./test/test_project/"
 		if exists(PATH):
 			self._remove_directory(PATH)
@@ -100,6 +100,127 @@ TEXT_FILE1
 end
 """)
 		file2.close()
+		self._remove_directory(PATH)
+		
+	def test_manually_2(self):
+		PATH = "./test/test_project/"
+		if exists(PATH):
+			self._remove_directory(PATH)
+		os.mkdir(PATH)
+		file = open(PATH + "file.txt", 'x')
+		file.write("""begin
+	some indentation:
+		/* ~ZMIJA.GENERATOR:
+		def declare(variables):
+			variables["first_list"] = []
+			pass
+			
+		def init(variables):
+			variables["second_list"].append("ONE")
+			variables["third_list"].append("ONE")
+			pass
+			
+		def generate(variables):
+			return "\\n".join(variables["first_list"])
+		*/// ~ZMIJA.GENERATED_CODE:
+		SOME GARBAGE
+		// ~ZMIJA.END
+upper middle
+	even more:
+		indentation:
+			/* ~ZMIJA.GENERATOR:
+			def declare(variables):
+				variables["second_list"] = []
+				pass
+				
+			def init(variables):
+				variables["first_list"].append("TWO")
+				variables["third_list"].append("TWO")
+				pass
+				
+			def generate(variables):
+				return "\\n".join(variables["second_list"])
+			*/// ~ZMIJA.GENERATED_CODE:
+			SOME GARBAGE
+			// ~ZMIJA.END
+lower middle
+/* ~ZMIJA.GENERATOR:
+def declare(variables):
+	variables["third_list"] = []
+	pass
+	
+def init(variables):
+	variables["first_list"].append("THREE")
+	variables["second_list"].append("THREE")
+	pass
+	
+def generate(variables):
+	return "\\n".join(variables["third_list"])
+*/// ~ZMIJA.GENERATED_CODE:
+SOME GARBAGE
+// ~ZMIJA.END
+end""")
+		file.close()
+		
+		Zmija.run(False, False, False, PATH)
+		
+		file = open(PATH + "file.txt", "r")
+		self.assertEqual(file.read(), """begin
+	some indentation:
+		/* ~ZMIJA.GENERATOR:
+		def declare(variables):
+			variables["first_list"] = []
+			pass
+			
+		def init(variables):
+			variables["second_list"].append("ONE")
+			variables["third_list"].append("ONE")
+			pass
+			
+		def generate(variables):
+			return "\\n".join(variables["first_list"])
+		*/// ~ZMIJA.GENERATED_CODE:
+		TWO
+		THREE
+		// ~ZMIJA.END
+upper middle
+	even more:
+		indentation:
+			/* ~ZMIJA.GENERATOR:
+			def declare(variables):
+				variables["second_list"] = []
+				pass
+				
+			def init(variables):
+				variables["first_list"].append("TWO")
+				variables["third_list"].append("TWO")
+				pass
+				
+			def generate(variables):
+				return "\\n".join(variables["second_list"])
+			*/// ~ZMIJA.GENERATED_CODE:
+			ONE
+			THREE
+			// ~ZMIJA.END
+lower middle
+/* ~ZMIJA.GENERATOR:
+def declare(variables):
+	variables["third_list"] = []
+	pass
+	
+def init(variables):
+	variables["first_list"].append("THREE")
+	variables["second_list"].append("THREE")
+	pass
+	
+def generate(variables):
+	return "\\n".join(variables["third_list"])
+*/// ~ZMIJA.GENERATED_CODE:
+ONE
+TWO
+// ~ZMIJA.END
+end""")
+		file.close()
 		self._remove_directory(PATH)
 		
 	
