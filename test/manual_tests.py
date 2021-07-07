68ae8f2f58e52bc9a176d1a6c7b2093308b912ca
Adrian Samoticha <redacted-for@privacy-reasons.com>
2021-07-07 20:15:30 +0200

    feat: add manual test

---
+++ test/manual_tests.py
@@ -0,0 +1,107 @@
+import unittest
+import os
+from os.path import exists
+import shutil
+import sys
+
+testdir = os.path.dirname(__file__)
+srcdir = '../src'
+sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
+
+from zmija import Zmija
+
+class ManualTest(unittest.TestCase):
+	def _remove_directory(self, path):
+		try:
+			shutil.rmtree(path)
+		except OSError as e:
+			print("Directory deletion failed: %s - %s." % (e.filename, e.strerror))
+	
+	def test_manually(self):
+		PATH = "./test/test_project/"
+		if exists(PATH):
+			self._remove_directory(PATH)
+		os.mkdir(PATH)
+		file1 = open(PATH + "file1.txt", 'x')
+		file1.write("""begin
+/* ~ZMIJA.GENERATOR:
+def declare(variables):
+	variables["file1_list"] = []
+	pass
+	
+def init(variables):
+	variables["file2_list"].append("TEXT_FILE1")
+	pass
+	
+def generate(variables):
+	return "\\n".join(variables["file1_list"])
+*/// ~ZMIJA.GENERATED_CODE:
+
+// ~ZMIJA.END
+end
+""")
+		file1.close()
+		file2 = open(PATH + "file2.txt", 'x')
+		file2.write("""begin
+/* ~ZMIJA.GENERATOR:
+def declare(variables):
+	variables["file2_list"] = []
+	pass
+	
+def init(variables):
+	variables["file1_list"].append("TEXT_FILE2")
+	pass
+	
+def generate(variables):
+	return "\\n".join(variables["file2_list"])
+*/// ~ZMIJA.GENERATED_CODE:
+
+// ~ZMIJA.END
+end
+""")
+		file2.close()
+		Zmija.run(False, False, False, PATH)
+		
+		file1 = open(PATH + "file1.txt", "r")
+		self.assertEqual(file1.read(), """begin
+/* ~ZMIJA.GENERATOR:
+def declare(variables):
+	variables["file1_list"] = []
+	pass
+	
+def init(variables):
+	variables["file2_list"].append("TEXT_FILE1")
+	pass
+	
+def generate(variables):
+	return "\\n".join(variables["file1_list"])
+*/// ~ZMIJA.GENERATED_CODE:
+TEXT_FILE2
+// ~ZMIJA.END
+end
+""")
+		file1.close()
+		file2 = open(PATH + "file2.txt", "r")
+		self.assertEqual(file2.read(), """begin
+/* ~ZMIJA.GENERATOR:
+def declare(variables):
+	variables["file2_list"] = []
+	pass
+	
+def init(variables):
+	variables["file1_list"].append("TEXT_FILE2")
+	pass
+	
+def generate(variables):
+	return "\\n".join(variables["file2_list"])
+*/// ~ZMIJA.GENERATED_CODE:
+TEXT_FILE1
+// ~ZMIJA.END
+end
+""")
+		file2.close()
+		self._remove_directory(PATH)
+		
+	
+if __name__ == '__main__':
+	unittest.main()
